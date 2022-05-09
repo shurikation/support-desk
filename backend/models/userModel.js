127f76b07e44c4001b1f87df4db6aca1543ee4f3
Alexander Popov <shurikation@gmail.com>
2022-05-09 19:18:43 +0400

    #17.9 - Register user

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+//User model for registration
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema({
+  //fields
+  name: {
+    type: String,
+    required: [true, 'Please add a name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password']
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+},
+{
+  timestamps: true,
+})
+
+module.exports = mongoose.model('User', userSchema)
