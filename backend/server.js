92eb8b5b717136ae849f77b76ac8f91a68049a70
Alexander Popov <shurikation@gmail.com>
2022-05-09 14:23:14 +0400

    #17.5 - Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  //This will display if we 'll send request to localhost:5000 in Postman
+  // res.send('Hello')
+  // res.json({message: 'LOL'})
+  res.status(200).json({message: 'Status 201 now :)'})
+})
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
