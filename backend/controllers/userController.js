5b9cb07e321b35e433af06f5fbc245968abafbe7
Alexander Popov <shurikation@gmail.com>
2022-05-09 20:10:34 +0400

    #17.11 - Protect Routes & Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,10 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs'); //to hash our password
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
 
-
-//@desc Register a new user
+//***@desc Register a new user
 //@route /api/users
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
@@ -42,24 +42,64 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user._id,
       name: user.name,
-      email: user. email
+      email: user. email,
+      token: generateToken(user._id)
     })
   } else { //If not created:
     res.sendStatus(400)
     throw new Error('Invalid user data')
   }
-  
+
 })
 
-
-//@desc Login a new user
+//***@desc Login a new user
 //@route /api/users/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route')
+
+  const {email, password} = req.body;
+  const user = await User.findOne({email})
+
+  //Check user && passwords match
+  if(user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user. email,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid credentials')
+  }
 })
+
+//***@desc Get current user
+//@route /api/users/me
+//@access Private
+const getMe = asyncHandler(async (req, res) => {
+  //If we wanna see just id, email, name in request
+  //Changing default keys for json - just copy data
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+  
+  res.status(200).json(user);
+})
+
+
+
+//Generate token (may be in separate file)
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d'
+  })
+}
 
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
+  getMe
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController')
+const { registerUser, loginUser, getMe } = require('../controllers/userController')
 
 //Before refactoring:
 // router.post('/', (req, res) => {
 //   res.send('Register Route')
 // })
 
+//If we wanna protect - we add this as a second argument to router method
+const {protect} = require('../middleware/authMiddleware')
+
 router.post('/', registerUser)
 router.post('/login', loginUser)
+router.get('/me', protect, getMe)
 
 module.exports = router;
