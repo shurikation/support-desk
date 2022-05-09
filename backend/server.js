f8c64e4a16263eb6de9a3ad8d1b03940540fb475
Alexander Popov <shurikation@gmail.com>
2022-05-09 18:24:31 +0400

    #17.8 - Connect To The Database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config();
-
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+// Connect to database
+connectDB();
+
+const app = express()
+
+//That helps to make request json to the server (from Postman, for example)
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 
 app.get('/', (req, res) => {
   //This will display if we 'll send request to localhost:5000 in Postman
@@ -14,5 +24,7 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
