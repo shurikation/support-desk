9f19afc0ac491061e62a43fc245af0086d86cd9d
Alexander Popov <shurikation@gmail.com>
2022-05-10 11:50:30 +0400

    #18.3 - Home, Login & Register UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Header from './components/Header'
+
 function App() {
   return (
     <>
@@ -16,6 +19,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
